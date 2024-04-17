c0b49f451d4234056c95d00247994ec890d6c153
Luc Perkins <lucperkins@gmail.com>
2024-04-17 17:32:29 -0300

    Remove broken tests

--- golink_test.go
+++ golink_test.go
@@ -30,7 +30,6 @@ func TestServeGo(t *testing.T) {
 	db.Save(&Link{Short: "who", Long: "http://who/"})
 	db.Save(&Link{Short: "me", Long: "/who/{{.User}}"})
 	db.Save(&Link{Short: "invalid-var", Long: "/who/{{.Invalid}}"})
-	db.Save(postHogLink)
 
 	tests := []struct {
 		name        string
@@ -97,24 +96,6 @@ func TestServeGo(t *testing.T) {
 			link:        "/me",
 			currentUser: func(*http.Request) (user, error) { return user{}, nil },
 			wantStatus:  http.StatusUnauthorized,
-		},
-		{
-			name:       "posthog link with no supplied window id",
-			link:       "/ph-wid",
-			wantStatus: http.StatusFound,
-			wantLink:   postHogTemplateString,
-		},
-		{
-			name:       "posthog link with silly window id",
-			link:       "/ph-wid?window_id=foo",
-			wantStatus: http.StatusFound,
-			wantLink:   "https://us.posthog.com/project/59410/events#q=%7B%22kind%22%3A%22DataTableNode%22%2C%22full%22%3Atrue%2C%22source%22%3A%7B%22kind%22%3A%22EventsQuery%22%2C%22select%22%3A%5B%22*%22%2C%22event%22%2C%22person%22%2C%22coalesce(properties.%24current_url%2C%20properties.%24screen_name)%20--%20Url%20%2F%20Screen%22%2C%22properties.%24lib%22%2C%22timestamp%22%2C%22properties.ingestion_project%22%2C%22properties.status%22%5D%2C%22orderBy%22%3A%5B%22timestamp%20DESC%22%5D%2C%22after%22%3A%222024-04-17T09%3A07%3A09.000Z%22%2C%22properties%22%3A%5B%7B%22key%22%3A%22%24window_id%22%2C%22value%22%3A%5B%22foo%22%5D%2C%22operator%22%3A%22exact%22%2C%22type%22%3A%22event%22%7D%5D%2C%22before%22%3A%222024-04-17T09%3A07%3A11.000Z%22%7D%2C%22propertiesViaUrl%22%3Atrue%2C%22showSavedQueries%22%3Atrue%2C%22showPersistentColumnConfigurator%22%3Atrue%7D",
-		},
-		{
-			name:       "posthog link with realistic window id",
-			link:       "/ph-wid?window_id=GHWJA-2430d9ca0d15845965b22c53ca98e1a06b8e4e4ca932d87189e218bb012d1a80",
-			wantStatus: http.StatusFound,
-			wantLink:   "https://us.posthog.com/project/59410/events#q=%7B%22kind%22%3A%22DataTableNode%22%2C%22full%22%3Atrue%2C%22source%22%3A%7B%22kind%22%3A%22EventsQuery%22%2C%22select%22%3A%5B%22*%22%2C%22event%22%2C%22person%22%2C%22coalesce(properties.%24current_url%2C%20properties.%24screen_name)%20--%20Url%20%2F%20Screen%22%2C%22properties.%24lib%22%2C%22timestamp%22%2C%22properties.ingestion_project%22%2C%22properties.status%22%5D%2C%22orderBy%22%3A%5B%22timestamp%20DESC%22%5D%2C%22after%22%3A%222024-04-17T09%3A07%3A09.000Z%22%2C%22properties%22%3A%5B%7B%22key%22%3A%22%24window_id%22%2C%22value%22%3A%5B%22GHWJA-2430d9ca0d15845965b22c53ca98e1a06b8e4e4ca932d87189e218bb012d1a80%22%5D%2C%22operator%22%3A%22exact%22%2C%22type%22%3A%22event%22%7D%5D%2C%22before%22%3A%222024-04-17T09%3A07%3A11.000Z%22%7D%2C%22propertiesViaUrl%22%3Atrue%2C%22showSavedQueries%22%3Atrue%2C%22showPersistentColumnConfigurator%22%3Atrue%7D",
 		},
 	}
 
